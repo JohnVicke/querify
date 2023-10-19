51d0ecffdb1c19f30a5e36c5c5a12888d4967499
ViktorM <viktor@malmedal.dev>
2023-10-19 10:07:15 +0200

    re export stuff

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,4 @@
 export { createClient } from "./client";
 export type { Client } from "./client";
+export type { CreateQueryOptions } from "./query";
+export { createQueryObserver, Observer } from "./observer";
