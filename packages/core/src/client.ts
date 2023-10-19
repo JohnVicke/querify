9d15a399e279a214956626cdfa56a6b34b95c92c
ViktorM <viktor@malmedal.dev>
2023-10-19 10:07:11 +0200

    update client

--- packages/core/src/client.ts
+++ packages/core/src/client.ts
@@ -1,9 +1,19 @@
 import { createQueryCache } from "./cache";
+import { hashKey, type CreateQueryOptions, createQuery, Query } from "./query";
 
 export function createClient() {
   const queryCache = createQueryCache();
 
   return {
+    get<TData>(options: CreateQueryOptions<TData>) {
+      const hashedKey = hashKey(options.key);
+      if (queryCache.has(hashedKey)) {
+        return queryCache.get(hashedKey) as Query<TData>;
+      }
+      const query = createQuery<TData>(this, options);
+      queryCache.add(hashedKey, query);
+      return query;
+    },
     mount() {
       console.log("client mounted");
     },
